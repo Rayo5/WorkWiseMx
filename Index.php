4f4f5922c6e7739fbf4bae400626b918e2bcf4d7
WorkWise <akilesdej@hotmail.com>
2024-09-29 15:33:41 -0600

    Se integro la carpeta autocontenedora con archivos

---
+++ Index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>WorkWise</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    </head>
+    <body>
+        <h1 class="text-center">WorkWise</h1>
+    <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
